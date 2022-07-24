3a9c9480191e06c515c4687b68a9f3b6c8935889
radekaadek <radekaadek@gmail.com>
2022-07-24 18:45:01 +0200

    +

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const NO_ROWS = 3
+const NO_COLLUMNS = 3
+const NO_ROUNDS = 10
+const numberOfSquares = NO_COLLUMNS*NO_ROWS
+let startTime;
+
 // Function to generate random number in a given range
 function randomNumber(min, max) { 
     return Math.floor(Math.random() * (max - min) + min);
@@ -8,36 +14,43 @@ function sleep(ms) {
  }
 
 function chooseNewID(currentID) {
-    let randomID = randomNumber(1, 10);
+    let randomID = randomNumber(1, numberOfSquares);
     while (currentID == randomID) {
-        randomID = randomNumber(1, 10);
+        randomID = randomNumber(1, numberOfSquares);
     }
     return randomID;
 }
 
 function removeTarget(target) {
-    target.setAttribute("src", "grey.png");
-    target.removeAttribute('onclick');
+    target.setAttribute("src", "images/grey.png");
+    target.removeAttribute('onmousedown');
 }
 
 function setNewTarget(target) {
-    target.setAttribute("src", "bunny.png");
-    target.setAttribute('onclick', 'clickOnBunny(this)');
+    target.setAttribute("src", "images/bunny.png");
+    target.setAttribute('onmousedown', 'clickOnBunny(this)');
 }
 
 async function clickOnBunny(){
     counter++;
     textBox.innerHTML = "Bunnies hit: " + counter.toString();
-    if (counter == 9) {
-        alert("You won!");
+    if (counter == NO_ROUNDS) {
+        let endTime = new Date().getTime();
+        alert("You won! your time was: " + 
+            ((endTime-startTime-waitTime)/1000).toString() + " seconds");
     }
-    newRandomID = chooseNewID(targetID);
-    console.log(newRandomID);
     removeTarget(targetItem);
-    await sleep(randomNumber(750, 2000));
-    targetItem = items[newRandomID-1]
-    setTimeout(setNewTarget(targetItem), randomNumber(1000, 2000));
+    sleepTime = randomNumber(750, 2000);
+    await sleep(sleepTime);
+    waitTime += startTime-sleepTime;
+    newRandomID = chooseNewID(targetID);
+    targetItem = items[newRandomID-1];
+    setNewTarget(targetItem);
     targetID = newRandomID;
+    if (counter == NO_ROUNDS) {
+        counter = 0;
+        play();
+    }
 }
 
 async function play() {
@@ -45,14 +58,15 @@ async function play() {
     textBox.innerHTML = "Bunnies hit: " + counter.toString();
     removeTarget(targetItem);
     await sleep(randomNumber(1000, 2000));
-    targetItem = items[chooseNewID(targetID)-1]
+    startTime = new Date().getTime();
+    targetItem = items[chooseNewID(targetID)-1];
     setNewTarget(targetItem);
 }
 
 let line = document.createElement('div');
-textBox = document.body.appendChild(line)
+textBox = document.body.appendChild(line);
 textBox.innerHTML = "Click on the rabbit to start the game!";
-textBox.setAttribute("class" ,"textBox")
+textBox.setAttribute("class" ,"textBox");
 
 // create a grid element
 const newDiv = document.createElement('div');
@@ -65,26 +79,28 @@ let item;
 let template;
 
 //draw empty squares
-for (var i = 1; i < 4; i++) {
+for (var i = 1; i < (NO_ROWS+1); i++) {
     let newBlock = document.createElement('div');
     newBlock.setAttribute("class", "block");
     newBlock.setAttribute("blockID", i);
     grid.appendChild(newBlock);
-    for (var j = 1; j < 4; j++) {
+    for (var j = 1; j < (NO_COLLUMNS+1); j++) {
         template = document.createElement('img');
         template.setAttribute("id", (i*3+j).toString());
         template.setAttribute("class", "targetIMG");
-        template.setAttribute("src", "grey.png");
+        template.setAttribute("src", "images/grey.png");
         item = newBlock.appendChild(template);
         items.push(item);
     }
 }
 
+
 let targetID = chooseNewID(-1);
 let newRandomID;
-let targetItem = items[targetID-1]
+let targetItem = items[targetID-1];
 setNewTarget(targetItem);
 
+let waitTime = 0;
 let counter = 0;
-targetItem.setAttribute("onclick",
+targetItem.setAttribute("onmousedown",
                         "play()");
